92f7e06b330da24f0755aa70e4bfbae49345e704
Julieta Rivarola <julietarivarola2@gmail.com>
2020-12-20 08:54:23 -0300

    Subo arquitectura base

---
+++ src/test/java/com/julir/springboot/CrudProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julir.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
